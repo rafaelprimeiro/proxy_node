bf6cbd92f253053282a2e2cf7b2ecad0325856f0
Rafael Gabriel <rafael.gabriel@dafiti.com.br>
2021-04-22 18:52:33 -0300

    initial

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Configuration
+const PORT = process.env.PORT || '3000';
+const API_SERVICE_URL = process.env.API_SERVICE_URL || "https://google.com";
+const CHANGE_ORIGIN = Boolean(process.env.CHANGE_ORIGIN) || true;
+
+//Debug
+app.use(morgan('dev'));
+
+app.get('/info', (req, res, next) => {
+    res.send('This is a proxy service which proxies to Billing and Account APIs.');
+ });
+
+//  app.use('', (req, res, next) => {
+//     if (req.headers.authorization) {
+//         next();
+//     } else {
+//         res.sendStatus(403);
+//     }
+//  });
+
+ app.use('/', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: CHANGE_ORIGIN,
+ }));
+
+ app.listen(PORT, () => {
+    console.log(`Starting Proxy at port ${PORT}`);
+ });
